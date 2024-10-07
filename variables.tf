a0f2e639097f964e7c86ac9e014d904aa20527e0
Mathesh Ramesh <ramesh.mat@northeastern.edu>
2024-10-07 10:00:11 -0400

    git actions and initial setup

---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "vpc_name" {
+  description = "Name for the VPC"
+  type        = string
+  default = "Main VPC"
+}
+
+variable "public_subnets" {
+  type = list(string)
+  default = ["10.0.1.0/24", "10.0.3.0/24", "10.0.5.0/24"]
+}
+
+variable "private_subnets" {
+  type = list(string)
+  default = ["10.0.2.0/24", "10.0.4.0/24", "10.0.6.0/24"]
+}
+
+variable "availability_zones" {
+  description = "Availability zones in the region"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c","us-east-1d", "us-east-1e", "us-east-1f"]
+}
