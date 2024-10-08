8188144cbddcab8e0f7704a695aee6813785f849
Mathesh Ramesh <ramesh.mat@northeastern.edu>
2024-10-07 22:35:05 -0400

    terraform formatted

--- variables.tf
+++ variables.tf
@@ -6,26 +6,26 @@ variable "vpc_cidr" {
 variable "vpc_name" {
   description = "Name for the VPC"
   type        = string
-  default = "Main vpc"
+  default     = "Main vpc"
 }
 
 variable "public_subnets" {
-  type = list(string)
+  type    = list(string)
   default = ["10.0.1.0/24", "10.0.3.0/24", "10.0.5.0/24"]
 }
 
 variable "private_subnets" {
-  type = list(string)
+  type    = list(string)
   default = ["10.0.2.0/24", "10.0.4.0/24", "10.0.6.0/24"]
 }
 
 variable "availability_zones" {
   description = "Availability zones in the region"
   type        = list(string)
-  default     = ["us-east-1a", "us-east-1b", "us-east-1c","us-east-1d", "us-east-1e", "us-east-1f"]
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1e", "us-east-1f"]
 }
 
-variable "cidr_block"{
-    description = "CIDR block for internet gateway"
-    default     = "0.0.0.0/0"
+variable "cidr_block" {
+  description = "CIDR block for internet gateway"
+  default     = "0.0.0.0/0"
 }
