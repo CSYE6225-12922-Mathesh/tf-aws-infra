997284bd74d22c199391f7cf9f46d7371ece729b
Mathesh Ramesh <ramesh.mat@northeastern.edu>
2024-10-31 01:05:20 -0400

    cloudwatch

--- main.tf
+++ main.tf
@@ -99,10 +99,10 @@ resource "aws_security_group" "app_sg" {
   }
 
   egress {
-    from_port   = 0             
-    to_port     = 0             
-    protocol    = "-1"          
-    cidr_blocks = ["0.0.0.0/0"] 
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
 
